f85ec517bba8e46c598f8e4629e2a4c9f266e49d
Sergio Cobos <43445037+sergioandresco@users.noreply.github.com>
2022-07-05 22:23:16 -0500

    Update fetch.js

--- Cliente/fetch.js
+++ Cliente/fetch.js
@@ -1,41 +1,69 @@
-const pantalla1 = document.getElementById('campo1');
-const pantalla2 = document.getElementById('campo2');
-
-//Listeners------------------------------------------------------
-document.getElementById("igual").addEventListener("click", function() { exe("igual"); });
+document.getElementById('igual').addEventListener('click', function() { exe("enviar"); });
+const botonesNumeros = document.getElementById('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
 
 function exe() {
     const data = new FormData();
-    if (arguments[0] == "igual") {
-        alert("Calculando");
-        data.append('igual', document.getElementById('igual').value);
+    if (arguments[0] == "enviar") {
+        alert("Enviando numero");
+        data.append('igual', document.getElementById('igual').textContent);
     }
 
-    data.append(pantalla1, document.getElementById('campo1').textContent);
+    data.append('campo2', document.getElementById('campo2').value);
+    data.append('campo1', document.getElementById('campo1').value);
 
     fetch(
-            'http://localhost/Calculadora/Servidor/Metodo.php',
+            '../Servidor/Metodo.php',
 
             {
                 method: 'POST',
                 body: data
             }
         )
-        .then(function(response) {
-            if (response.ok) {
-                return response.text()
-                    //console.log(response.json())
-            } else {
-                throw "Error en la llamada Fetch";
-            }
-
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
         })
-        .then(function(respuestaDelServidor) {
-            document.getElementById('resp').innerHTML = respuestaDelServidor
-            console.log(respuestaDelServidor);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-
 }
+
+
+
+/*
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log('enviaste la operacion');
+
+    var datos1 = new FormData(displayValorAnterior);
+
+    console.log(datos1);
+
+})
+*/
+
+/*
+var formulario = document.getElementById('formulario');
+const pantalla1 = document.getElementById('pantalla1');
+const pantalla2 = document.getElementById('pantalla2');
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log('enviaste la operacion');
+
+    var datos1 = new FormData(pantalla1);
+    var datos2 = new FormData(pantalla2);
+
+    console.log(datos1);
+    console.log(datos2);
+
+    /*
+    fetch('http://localhost/Calculadora/Servidor/Metodo.php', {
+        method: 'POST',
+        body: datos
+    })
+
+    .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        */
+//})
